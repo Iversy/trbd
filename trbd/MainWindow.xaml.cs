c5f5c094a8bbb417da76d5adbfbfa27534f40e72
Iversy <iversy49@ya.ru>
2024-09-23 17:12:38 +0500

    Скрыт столбец FK_Material_Usage, обработана ошибка при сохранении пустого файла без разметки

--- trbd/MainWindow.xaml.cs
+++ trbd/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,13 +26,21 @@ namespace trbd
 
         private void On_Save_Button_Click(object sender, RoutedEventArgs e)
         {
-            app.stupid_data.WriteXml("./stupid_data.xml");
+            try
+            {
+                app.stupid_data.WriteXml("./stupid_data.xml");
+            } 
+            catch (NullReferenceException exception)
+            {
+                MessageBox.Show("Отсутствуют данные для сохранения, загрузите данные, чтобы начать изменения.\n" + exception.Message, "Нет данных.", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void On_Load_Button_Click(object sender, RoutedEventArgs e)
         {
             app.load_data();
             MaterialGrid.ItemsSource = app.stupid_data.Tables["Material"].DefaultView;
+            MaterialGrid.Columns[4].Visibility = Visibility.Collapsed;
             UsageGrid.ItemsSource = app.stupid_data.Tables["Usage"].DefaultView;
         }
     }
